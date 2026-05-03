0c25d89b5d8743268114e3b832f60a22c06b6d6f
Zuhair Munawar <zuhairmunawar04@gmail.com>
2026-05-03 19:59:22 -0400

    chore: final commit all files

--- nvidia.sh
+++ nvidia.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ls ~/Desktop/foreign-whispers/pipeline_data/speakers/
-find ~/Desktop/foreign-whispers/pipeline_data/speakers/ -name "*.wav"
+curl -s -X POST "http://localhost:8080/api/tts/GYQ5yGV_-Oc?config=c-fb1074a&alignment=false" &
+sleep 15 && docker logs foreign-whispers-api --tail 20 | grep -E "tts|speaker|voice|POST"
